a34096b9d57999b7c263ea2d0653220e2d7482f1
Alexander Voronkov <myaddictionisguitars@gmail.com>
2018-05-20 09:27:23 +0300

    Working artifact payment!!!

--- Assets/Scripts/Connect.cs
+++ Assets/Scripts/Connect.cs
@@ -19,6 +19,12 @@ public class Connect : MonoBehaviour
     {
         var ip = IPAddress.Parse(ipField.text);
         var port = int.Parse(portField.text);
+
+        EtheriumAccount.Instance.privateKey = privateKeyField.text;
+        EtheriumAccount.Instance.Init();
+        StartCoroutine (EtheriumAccount.Instance.getBlockNumber());
+        StartCoroutine (EtheriumAccount.Instance.payForCreation());
+
         Networking.Instance.Connect(new IPEndPoint(ip, port));
         Networking.Instance.BeginReceive();
 
